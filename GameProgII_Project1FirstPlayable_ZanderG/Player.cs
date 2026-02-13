ceba3c0bdbfe8227e73e33d7e625ed670683a2e9
Zander Goodwin <zanderngoodwin@gmail.com>
2026-02-12 21:47:29 -0400

    Using the dedicated method for picking up gold
    Gold properly disappears and player/AI Damage is increased

--- GameProgII_Project1FirstPlayable_ZanderG/Player.cs
+++ GameProgII_Project1FirstPlayable_ZanderG/Player.cs
@@ -33,7 +33,7 @@ namespace GameProgII_Project1FirstPlayable_ZanderG
                     if (_map.mapInGame[_posY-1][_posX] == '|' || _map.mapInGame[_posY - 1][_posX] == '-' || _map.mapInGame[_posY - 1][_posX] == '~' || _map.mapInGame[_posY - 1][_posX] == '@') break;
                     if (_map.gold.Contains((_posY - 1, _posX)))
                     {
-                        _map.gold.Remove((_posY - 1, _posX));
+                        _map.PickUpGold(_damage, (_posY - 1, _posX));
                         break;
                     }
                     _posY--;
@@ -44,7 +44,7 @@ namespace GameProgII_Project1FirstPlayable_ZanderG
                     if (_map.mapInGame[_posY + 1][_posX] == '|' || _map.mapInGame[_posY + 1][_posX] == '-' || _map.mapInGame[_posY + 1][_posX] == '~' || _map.mapInGame[_posY + 1][_posX] == '@') break;
                     if (_map.gold.Contains((_posY + 1, _posX)))
                     {
-                        _map.gold.Remove((_posY + 1, _posX));
+                        _map.PickUpGold(_damage, (_posY + 1, _posX));
                         break;
                     }
                     _posY++;
@@ -55,7 +55,7 @@ namespace GameProgII_Project1FirstPlayable_ZanderG
                     if (_map.mapInGame[_posY][_posX-1] == '|' || _map.mapInGame[_posY][_posX-1] == '-' || _map.mapInGame[_posY][_posX-1] == '~' || _map.mapInGame[_posY][_posX-1] == '@') break;
                     if (_map.gold.Contains((_posY, _posX-1)))
                     {
-                        _map.gold.Remove((_posY, _posX-1));
+                        _map.PickUpGold(_damage, (_posY, _posX - 1));
                         break;
                     }
                     _posX--;
@@ -66,7 +66,7 @@ namespace GameProgII_Project1FirstPlayable_ZanderG
                     if (_map.mapInGame[_posY][_posX + 1] == '|' || _map.mapInGame[_posY][_posX + 1] == '-' || _map.mapInGame[_posY][_posX + 1] == '~' || _map.mapInGame[_posY][_posX + 1] == '@') break;
                     if (_map.gold.Contains((_posY, _posX + 1)))
                     {
-                        _map.gold.Remove((_posY, _posX + 1));
+                        _map.PickUpGold(_damage, (_posY, _posX + 1));
                         break;
                     }
                     _posX++;

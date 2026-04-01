eabc3832236ed575c61289bf4b357c460b8e2aec
Zander Goodwin <zanderngoodwin@gmail.com>
2026-03-31 22:09:52 -0300

    Game Loop Implemented

--- GameProgII_Project1FirstPlayable_ZanderG/Program.cs
+++ GameProgII_Project1FirstPlayable_ZanderG/Program.cs
@@ -11,72 +11,78 @@ namespace GameProgII_Project1FirstPlayable_ZanderG
     {
         static void Main(string[] args)
         {
-            Map map = new Map();
-            Hud hud = new Hud();
-            CollectablesSpawner collectables = new CollectablesSpawner();
-
-            int numberOfStandardEnemies = 25;
-
-            Player player = new Player(hp: 10, posX: 0, posY: 0, damage: 1, gameMap: map, lastEncounteredEnemy: 0);
-            Enemy enemyR1 = new RandomEnemy(hp: 6, posX: 17, posY: 0, damage: 3, gameMap: map, icon: 'R');
-            Enemy quickEnemy = new QuickEnemy(hp: 6, posX: 0, posY: 11, damage: 1, gameMap: map, icon: '2'); //Boss Enemy
-            Enemy enemyR2 = new RandomEnemy(hp: 6, posX: 10, posY: 6, damage: 3, gameMap: map, icon: 'R');
-
-            List<Enemy> enemies = new List<Enemy> { quickEnemy, enemyR1, enemyR2 };
-
-            for(int i = 0; i < numberOfStandardEnemies; i++)
-            {
-                enemies.Add(new Enemy(hp: 1, posX: 17, posY: 11, damage: 1, gameMap: map, icon: '#')); //Putting all weak enemies in the same spot to mimic a spawner
-            }
-
-            map.mapColors.Add('*', ConsoleColor.Green);
-            map.mapColors.Add('~', ConsoleColor.Blue);
-            map.mapColors.Add('+', ConsoleColor.Magenta);
-            map.mapColors.Add('=', ConsoleColor.DarkRed);
-
             bool isPlaying = true;
 
-            collectables.GetCollectableLocations(map);
-
-            map.PrintMap(collectables.gold.listOfGold, collectables.healthUp.listOfHealthUp, collectables.healthMax.listOfHealthMax);
-            DrawPlayers(player, enemies);
-
-            hud.ChangeEventLog("Game has started", map);
             while (isPlaying)
             {
-                hud.PrintHUD("Player's Turn", player, enemies, map);
+                Map map = new Map();
+                Hud hud = new Hud();
+                CollectablesSpawner collectables = new CollectablesSpawner();
 
-                ConsoleKey playerInput = Console.ReadKey(true).Key;
-                player.Move(playerInput, enemies, collectables.gold.listOfGold, collectables.healthUp.listOfHealthUp, collectables.healthMax.listOfHealthMax, hud);
-                map.PrintMap(collectables.gold.listOfGold, collectables.healthUp.listOfHealthUp, collectables.healthMax.listOfHealthMax);
-                DrawPlayers(player, enemies);
+                int numberOfStandardEnemies = 25;
 
-                for (int i = 0; i < enemies.Count; i++)
+                Player player = new Player(hp: 10, posX: 0, posY: 0, damage: 1, gameMap: map, lastEncounteredEnemy: 0);
+                Enemy enemyR1 = new RandomEnemy(hp: 6, posX: 17, posY: 0, damage: 3, gameMap: map, icon: 'R');
+                Enemy quickEnemy = new QuickEnemy(hp: 6, posX: 0, posY: 11, damage: 1, gameMap: map, icon: '2'); //Boss Enemy
+                Enemy enemyR2 = new RandomEnemy(hp: 6, posX: 10, posY: 6, damage: 3, gameMap: map, icon: 'R');
+
+                List<Enemy> enemies = new List<Enemy> { quickEnemy, enemyR1, enemyR2 };
+
+                for (int i = 0; i < numberOfStandardEnemies; i++)
                 {
-                    hud.PrintHUD($"Enemy{i + 1}'s Turn", player, enemies, map);
-                    Thread.Sleep(10);
-                    enemies[i].Move(player, enemies, i, collectables.gold.listOfGold, collectables.healthUp.listOfHealthUp, collectables.healthMax.listOfHealthMax, hud);
+                    enemies.Add(new Enemy(hp: 1, posX: 17, posY: 11, damage: 1, gameMap: map, icon: '#')); //Putting all weak enemies in the same spot to mimic a spawner
                 }
+
+                map.mapColors.Add('*', ConsoleColor.Green);
+                map.mapColors.Add('~', ConsoleColor.Blue);
+                map.mapColors.Add('+', ConsoleColor.Magenta);
+                map.mapColors.Add('=', ConsoleColor.DarkRed);
+
+                bool currentGameLoop = true;
+
+                collectables.GetCollectableLocations(map);
+
                 map.PrintMap(collectables.gold.listOfGold, collectables.healthUp.listOfHealthUp, collectables.healthMax.listOfHealthMax);
                 DrawPlayers(player, enemies);
 
-                isPlaying = CheckIfGameOver(player, enemies);
-
-                if (!isPlaying)
+                hud.ChangeEventLog("Game has started", map);
+                while (currentGameLoop)
                 {
-                    Console.Clear();
-                    if (player._health.health == 0)
+                    hud.PrintHUD("Player's Turn", player, enemies, map);
+
+                    ConsoleKey playerInput = Console.ReadKey(true).Key;
+                    player.Move(playerInput, enemies, collectables.gold.listOfGold, collectables.healthUp.listOfHealthUp, collectables.healthMax.listOfHealthMax, hud);
+                    map.PrintMap(collectables.gold.listOfGold, collectables.healthUp.listOfHealthUp, collectables.healthMax.listOfHealthMax);
+                    DrawPlayers(player, enemies);
+
+                    for (int i = 0; i < enemies.Count; i++)
                     {
-                        Console.WriteLine("Game Over! You lose.");
-                        Console.ReadKey(true);
+                        hud.PrintHUD($"Enemy{i + 1}'s Turn", player, enemies, map);
+                        Thread.Sleep(10);
+                        enemies[i].Move(player, enemies, i, collectables.gold.listOfGold, collectables.healthUp.listOfHealthUp, collectables.healthMax.listOfHealthMax, hud);
                     }
-                    else
+                    map.PrintMap(collectables.gold.listOfGold, collectables.healthUp.listOfHealthUp, collectables.healthMax.listOfHealthMax);
+                    DrawPlayers(player, enemies);
+
+                    currentGameLoop = CheckIfGameOver(player, enemies);
+                    if (playerInput == ConsoleKey.Escape) currentGameLoop = false; //Debug to get to end
+
+                    if (!currentGameLoop)
                     {
-                        Console.WriteLine("Congratulations! You win!");
-                        Console.ReadKey(true);
+                        Console.Clear();
+                        if (player._health.health == 0)
+                        {
+                            Console.WriteLine("You lose.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        Console.WriteLine("Play Again? (Y/N)");
+                        ConsoleKey playAgainInput = Console.ReadKey(true).Key;
+                        if (playAgainInput == ConsoleKey.Y) isPlaying = true;
+                        else isPlaying = false;
                     }
-                    Console.ReadKey(true);
-                    Console.ReadKey(true);
                 }
             }
         }
